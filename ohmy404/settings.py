8659c7cd80bdd74eb1c14c5b32c778bfd6f0b24e
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-05 19:05:14 -0400

    adding todos

--- ohmy404/settings.py
+++ ohmy404/settings.py
@@ -110,12 +110,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
 
 IGNORABLE_404_URLS = (
     re.compile(r'\.(php|cgi)$'),
